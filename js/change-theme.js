d3e5adf270ef3f1fd1291f12e47e38bcc0d3e2fe
martasoricetti <marta.soricetti@studio.unibo.it>
2022-09-06 17:30:06 +0200

    Update change-theme.js

--- js/change-theme.js
+++ js/change-theme.js
@@ -3,7 +3,10 @@ function changeTheme(id) {
     const theme = document.getElementsByTagName('link')[1];
     switch (id) {
         case "enable-default":
-            theme.href = "/styles/default.css";
+            theme.href = "styles/default.css";
+            break;
+        case "enable-pop-art":
+            theme.href = "styles/pop.css";
             break;
     }
   }
